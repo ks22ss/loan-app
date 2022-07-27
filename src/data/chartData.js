649e01e1825107e3ada74e23f2f62d37c2d6ef75
ks22ss <107049203+ks22ss@users.noreply.github.com>
2022-07-27 20:04:35 +0800

    add charts

---
+++ src/data/chartData.js
@@ -0,0 +1,12 @@
+const chartData = [
+    {year: '2014', loanTotal: 100, loanRequest: 2700},
+    {year: '2015', loanTotal: 200, loanRequest: 3100},
+    {year: '2016', loanTotal: 400, loanRequest: 3650},
+    {year: '2017', loanTotal: 800, loanRequest: 4700},
+    {year: '2018', loanTotal: 1600, loanRequest: 6890},
+    {year: '2019', loanTotal: 3200, loanRequest: 8950},
+    {year: '2020', loanTotal: 6400, loanRequest: 12530},
+    {year: '2021', loanTotal: 12800, loanRequest: 18700},
+];
+
+export {chartData};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,54 @@
 import "../css/Home.css"
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, LabelList, Tooltip } from 'recharts';
+import {chartData} from "../data/chartData"
+import "../css/Button.css"
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+
 const Home = () => {
+
     return ( 
-        <div className="container">
-            <section className="">
-                <h1>Demo Version</h1>
+        <div>
+            <section className="welcomeSession">
+               <h1 className="welcomeTitle">立即網上申請可享高達$18,000現金獎</h1>
+               <Link className="default-button home-button" to="/loan-request">立即申請</Link>
             </section>
+            <section className="loanTotalSession">
+                <div className="loanTotalSessionContainer">
+                    <h1 className="chartTitle">每年物業按揭申請貸款總額(百萬)</h1>
+                    <ResponsiveContainer width="70%" height={300}>
+                        <BarChart  data={chartData}>
+                            <XAxis dataKey="year"/>
+                            <YAxis />
+                            <Tooltip />
+                            <Bar dataKey="loanTotal" barSize={40} fill="#2F9933" >
+                                <LabelList dataKey="loanTotal" position="top" />
+                            </Bar>
+                        </BarChart>
+                    </ResponsiveContainer>
+                    
+                </div>   
+            </section>
+            <section className="loanRequestSession">
+                <div className="loanTotalSessionContainer">
+                        <h1 className="chartTitle">每年物業按揭申請次數</h1>
+                        <ResponsiveContainer width="70%" height={300}>
+                            <BarChart  data={chartData}>
+                                <XAxis dataKey="year"/>
+                                <YAxis />
+                                <Tooltip />
+                                <Bar dataKey="loanRequest" barSize={40} fill="#6CC013" >
+                                    <LabelList dataKey="loanRequest" position="top" />
+                                </Bar>
+                            </BarChart>
+                        </ResponsiveContainer>
+                </div>  
+            </section>
+            <section>
+
+            </section>
+            
         
         </div>
      );
